a0a599a6967506bf55b44c598b937db54325c4cf
ScriptKiddingBaby <scriptkidding1@gmail.com>
2023-10-10 17:51:55 +0800

    adding error validation for routessss!

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,10 @@
-<a href="/">Back</a>
+<script lang="ts">
+    import { page } from "$app/stores";
+</script>
+
+<main class="min-h-screen flex flex-col justify-center items-center">
+    <h3 class="h3 text-red-500">Error at {$page.status} {$page.error?.message}</h3>
+    <p class="">What are you doing step bro? this is <b>so wrong!</b></p>
+    <a href="/" class="mt-10 text-blue-500 underline active:scale-95 hover:text-red-500">Come to home page dadie!</a>
+
+</main>
